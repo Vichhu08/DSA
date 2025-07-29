fa5fadd63dccec5d180f4dc44224e1f5dd470f38
VISWANATHAN B R <145002896+Vichhu08@users.noreply.github.com>
2025-07-29 21:02:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int mid = 0, low = 0, high = n-1;
+        
+        while( mid <= high ){
+            if(arr[mid] == 0){
+                swap(arr[mid], arr[low]);
+                mid++;
+                low++;
+            }
+            
+            else if(arr[mid] == 1){
+                mid++;
+            }
+            
+            else{
+                swap(arr[mid], arr[high]);
+                high--;
+            }
+        }
+    }
+};
