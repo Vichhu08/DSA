a5086565f04632058acb23c58000ff73ffbbcf8d
VISWANATHAN B R <145002896+Vichhu08@users.noreply.github.com>
2025-11-01 09:59:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int> &arr) {
+        // Code here
+        long long sum =0, max = LONG_MIN;
+        
+        for(int i = 0; i<arr.size(); i++){
+            sum += arr[i];
+            if ( sum > max) max = sum;
+            if (sum < 0) sum = 0;
+        }
+        return max;
+        
+    }
+};
