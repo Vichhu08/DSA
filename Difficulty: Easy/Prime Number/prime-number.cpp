221c4d96e16382ad3bdcdbee2dacea3031c8db52
VISWANATHAN B R <145002896+Vichhu08@users.noreply.github.com>
2025-11-09 21:08:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.cpp
+++ Difficulty: Easy/Prime Number/prime-number.cpp
@@ -3,7 +3,7 @@ class Solution {
     bool isPrime(int n) {
         // code here
     int br = 0;
-    for (int i = 2; i < n/2; i++){
+    for (int i = 2; i*i <= n; i++){
         if(n % i == 0){
             br = 1;
             break;
